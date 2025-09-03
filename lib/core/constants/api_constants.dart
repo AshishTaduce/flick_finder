2a2d7821c6072b44ef5f46340b7dfbfecdd7a30d
Ashish <ashishtaduce2@gmail.com>
2025-09-03 17:15:44 +0530

    style: Add TODO for API key management
    Added a TODO comment to fetch the API key from environment variables instead of hardcoding it.

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,8 +1,7 @@
 class ApiConstants {
   static const String baseUrl = 'https://api.themoviedb.org/3';
   static const String imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
-  static const String apiKey = '997363e638a3ab108c2b55f53bd585fa'; // Replace with your actual API key
-
+  static const String apiKey = '997363e638a3ab108c2b55f53bd585fa'; ///TODO: Fetch from .env
   // Endpoints
   static const String popularMovies = '/movie/popular';
   static const String topRatedMovies = '/movie/top_rated';
